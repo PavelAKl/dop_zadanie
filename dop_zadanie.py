892815859186f690f76e7f97b97da81270ce7b6a
PavelAKl <pavelandreevichk98@gmail.com>
2024-12-20 01:53:51 +0300

    Add files via upload

--- dop_zadanie.py
+++ dop_zadanie.py
@@ -1,11 +1,8 @@
 grades = [[5,3,3,5,4],[2,2,2,3],[4,5,5,2],[4,4,3],[5,5,5,4,5]]
 students = {'Johnny','Bilbo','Steve','Khendrik','Aaron'}
-a = (sum(grades[0])/len(grades[0]))
-b = (sum(grades[1])/len(grades[1]))
-j = (sum(grades[2])/len(grades[2]))
-k = (sum(grades[3])/len(grades[3]))
-s = (sum(grades[-1])/len(grades[-1]))
-f = [a,b,j,k,s]
+f = [sum(grades[i])/len(grades[i]) for i in range(len(grades))]
 l = sorted(list(students))
 e = dict(zip(l,f))
 print(e)
+sred = {l[i]:f[i] for i in range(len(l))}
+print(sred)
